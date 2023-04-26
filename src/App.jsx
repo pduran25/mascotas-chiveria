085c294d140d4b2086b87442bd3e8afc91b12b00
rubikinteractive <pedroduran.25@gmail.com>
2023-04-25 20:53:16 -0500

    prueba

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import styled from 'styled-components';
+
 import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
+  const search = window.location.search;
+  const params = new URLSearchParams(search);
+  const codigo = params.get('mascota');
+  console.log("mascota numero: "+codigo);
+  var source = "";
+
+  source = "./assets/animacionpreg.glb"
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+       <model-viewer src={source} autoplay ar ar-modes="webxr scene-viewer quick-look" camera-controls touch-action="pan-y" camera-orbit="0deg 80deg 0m" alt="A 3D model of some wall art">
+        <Boton slot="ar-button">
+          🎉 RA 🎉
+        </Boton>
+      </model-viewer>
+    </div>
   )
 }
+
+const Boton = styled.button`
+background-color: white; 
+border-radius: 4px; 
+border: none; 
+position: absolute; 
+bottom: 50px; 
+right: 100px; 
+color: black;
+font-size: 16px;
+
+
+`;
 
 export default App
