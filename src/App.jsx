4b05a40d25cf24832dcfb8c7fd61dc6902ed3dfa
pduran25 <43051206+pduran25@users.noreply.github.com>
2023-06-20 23:56:59 -0500

    20junio2023

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import sound from './assets/chiverito.mp3';
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 
 import './App.css'
@@ -12,34 +12,57 @@ function App() {
   const codigo = params.get('mascota');
   console.log("mascota numero: "+codigo);
   var source = "";
+  var source2 = "";
   const [mostrarDiv, setMostrarDiv] = useState(true);
+  const [isAudioPlaying, setIsAudioPlaying] = useState(false);
+
 
   function play(){
     new Audio(sound).play();
+   // alert('¡Hola! Esto es un mensaje de alerta.');
+   
     setMostrarDiv(false);
   }
+
+  
 
  
   
 if(codigo == "chiverito"){
   source = "./assets/chiverito.glb"
+  source2 = "./assets/chive.usdz"
 }
-  
+
+const toggleAudio = () => {
+  const audioElement = document.getElementById('myAudio');
+
+  if (isAudioPlaying) {
+    audioElement.pause();
+  } else {
+    audioElement.play();
+  }
+
+  setIsAudioPlaying(!isAudioPlaying);
+};
+
 
   return (
     <div className="App">
-     
+<model-viewer src={source} ios-src={source2} ar ar-modes="webxr scene-viewer quick-look" ar-scale="auto" camera-controls shadow-intensity="1"  camera-target="0m 0m 0m" autoplay>
 
-<model-viewer src={source} ar ar-modes="webxr scene-viewer quick-look" camera-controls poster="poster.webp" shadow-intensity="1" autoplay>
-{mostrarDiv ? (<div id="color-controls">
-          <button data-color="#ff0000" onClick={play}>SONIDO</button>
-        </div>): (<div id="color-controls">
-        
-        </div>)}
-       
+        <SoundButton
+        src={isAudioPlaying ? './assets/audio.png' : './assets/sinaudio.png'}
+        alt={isAudioPlaying ? 'Sonido inactivo' : 'Sonido activo'}
+        onClick={toggleAudio}
+      />
+
+      <audio id="myAudio" src={sound} />
+
+
         <Boton slot="ar-button">
-          🎉 RA 🎉
+          Click Aquí
         </Boton>
+
 
         
   </model-viewer>
@@ -47,17 +70,72 @@ if(codigo == "chiverito"){
   )
 }
 
-const Boton = styled.button`
-background-color: white; 
-border-radius: 4px; 
-border: none; 
-position: absolute; 
-bottom: 50px; 
-right: 100px; 
-color: black;
+const SoundButton = styled.img`
+display: inline-block;
+`;
+
+
+
+
+
+const BotonB = styled.button`
+
+display: inline-block;
+border: none;
+border-radius: 4px;
+background-color: #0088cc;
+color: #fff;
+padding: 12px 24px;
 font-size: 16px;
+font-weight: bold;
+text-align: center;
+text-decoration: none;
+cursor: pointer;
+box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+transition: background-color 0.3s ease;
 
 
+bottom: 100px; 
+
+&:hover {
+  background-color: #006699;
+}
+
+&:focus {
+  outline: none;
+  background-color: #004466;
+}
+`;
+
+const Boton = styled.button`
+
+display: inline-block;
+border: none;
+border-radius: 4px;
+background-color: #0088cc;
+color: #fff;
+padding: 12px 24px;
+font-size: 16px;
+font-weight: bold;
+text-align: center;
+text-decoration: none;
+cursor: pointer;
+box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+transition: background-color 0.3s ease;
+position: absolute; 
+right: 100px; 
+left: 100px;
+
+bottom: 100px; 
+
+&:hover {
+  background-color: #006699;
+}
+
+&:focus {
+  outline: none;
+  background-color: #004466;
+}
 `;
 
 export default App;
