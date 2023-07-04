d1b17d1aa8fe50a4753406a8e5f2ae93073bfa37
pduran25 <43051206+pduran25@users.noreply.github.com>
2023-07-04 12:43:28 -0500

    4 julio 2023

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import sound from './assets/chiverito.mp3';
-import React, { useState, useEffect, useRef } from 'react';
+import  { useState} from 'react';
 
 
 import './App.css'
@@ -13,24 +13,16 @@ function App() {
   console.log("mascota numero: "+codigo);
   var source = "";
   var source2 = "";
-  const [mostrarDiv, setMostrarDiv] = useState(true);
   const [isAudioPlaying, setIsAudioPlaying] = useState(false);
 
 
-  function play(){
-    new Audio(sound).play();
-   // alert('¡Hola! Esto es un mensaje de alerta.');
-   
-    setMostrarDiv(false);
-  }
 
   
 
- 
   
 if(codigo == "chiverito"){
   source = "./assets/chiverito.glb"
-  source2 = "./assets/chive.usdz"
+  source2 = "./assets/chiverito.usdz"
 }
 
 const toggleAudio = () => {
@@ -48,7 +40,7 @@ const toggleAudio = () => {
 
   return (
     <div className="App">
-<model-viewer src={source} ios-src={source2} ar ar-modes="webxr scene-viewer quick-look" ar-scale="auto" camera-controls shadow-intensity="1"  camera-target="0m 0m 0m" autoplay>
+<model-viewer src={source} ios-src={source2} ar ar-modes="webxr scene-viewer quick-look" ar-scale="auto" camera-controls camera-target="0m 0m 0m" autoplay>
 
         <SoundButton
         src={isAudioPlaying ? './assets/audio.png' : './assets/sinaudio.png'}
@@ -78,34 +70,7 @@ display: inline-block;
 
 
 
-const BotonB = styled.button`
 
-display: inline-block;
-border: none;
-border-radius: 4px;
-background-color: #0088cc;
-color: #fff;
-padding: 12px 24px;
-font-size: 16px;
-font-weight: bold;
-text-align: center;
-text-decoration: none;
-cursor: pointer;
-box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
-transition: background-color 0.3s ease;
-
-
-bottom: 100px; 
-
-&:hover {
-  background-color: #006699;
-}
-
-&:focus {
-  outline: none;
-  background-color: #004466;
-}
-`;
 
 const Boton = styled.button`
 
