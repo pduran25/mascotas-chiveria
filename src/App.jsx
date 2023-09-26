b9769195bca7473271fef71670d5025e74e77adb
pduran25 <43051206+pduran25@users.noreply.github.com>
2023-09-25 22:49:49 -0500

    sincroniza 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import sound from './assets/chiverito.mp3';
-import  {useEffect,  useState} from 'react';
+import  {useState} from 'react';
 
 
 import './App.css'
